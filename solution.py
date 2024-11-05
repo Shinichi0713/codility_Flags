e46aa65506beb5e74573d9d5e9eac9263a8ee086
Shinichi Yoshinaga <yoshishinnze@yahoo.co.jp>
2024-11-06 06:13:42 +0900

    run

--- solution.py
+++ solution.py
@@ -1,18 +1,76 @@
+# max flagsの計算
 def solution(A):
     N = len(A)
-    peaks = [0] * (N + 2)
-    for i in range(1, N + 1):
-        peaks[i] = peaks[i - 1] + (0 < i - 1 < N - 1 and A[i - 2] < A[i - 1] > A[i])
-
-    for distance in range(N, 0, -1):
-        flags = 0
-        pos = 1
-        while pos <= N and flags < distance:
-            if peaks[min(pos + distance - 1, N)] > peaks[pos - 1]:
+    peaks = []
+    distance_between = []
+    for i in range(1, N):
+        if A[i] >= A[i - 1] and A[i] >= A[i+1]:
+            peaks.append(i)
+    for i in range(1, len(peaks)):
+        distance_between.append(peaks[i] - peaks[i-1])
+    peak_max = len(peaks)
+    flags = 1
+    for distance in range(1, peak_max):
+        if len(peaks) < distance:
+            break
+        # peakの数がdistanceより少ない場合は、distanceの数を返す
+        while True:
+            for i in range(1, len(peaks)):
+                if peaks[i] - peaks[i-1] < distance:
+                    peaks, distance_between = calculate_redistance(peaks, distance_between, distance)
+                    break
+            if i == len(peaks) - 1:
                 flags += 1
-                pos += distance
-            else:
-                pos += 1
-        if flags == distance:
-            return flags
-    return 0
+                break
+            if i == 1:
+                break
+    return flags
+
+# 距離の調整を行う
+def calculate_redistance(peaks, distance_between, distance_restrict):
+    peaks_rearranged = []
+    distance_between_rearranged = []
+    # dist_min = min(distance_between)
+    # is_rearranged = False
+    for i in range(len(distance_between)):
+        if distance_between[i-1] < distance_restrict:
+            pass
+        elif distance_between[i-1] >= distance_restrict:
+            peaks_rearranged.append(peaks[i-1])
+            distance_between_rearranged.append(peaks[i] - peaks[i-1])
+    return peaks_rearranged, distance_between_rearranged
+
+def solution_old(A):
+    N = len(A)
+    peaks = [0] * (N + 1)
+    for i in range(1, N):
+        peaks[i] = peaks[i - 1] + (0 if i == 1 or i == N - 1 or A[i - 1] <= A[i - 2] or A[i - 1] <= A[i] else 1)
+    peak_max = max(peaks)
+    print(peaks)
+    for distance in range(peak_max):
+        flags = 1
+        pos = 1
+        while flags < distance:
+            if pos + distance > N + 1 or peaks[pos + distance] <= flags:
+                break
+            pos += distance
+            flags += 1
+        else:
+            return distance
+    return peaks[N]
+
+if __name__ == '__main__':
+    A=[0 for _ in range(12)]
+    A[0] = 1
+    A[1] = 5
+    A[2] = 3
+    A[3] = 4
+    A[4] = 3
+    A[5] = 4
+    A[6] = 1
+    A[7] = 2
+    A[8] = 3
+    A[9] = 4
+    A[10] = 6
+    A[11] = 2
+    print(solution(A))
---
+++ test.py
@@ -0,0 +1,33 @@
+import unittest
+from solution import solution
+
+class TestSolution(unittest.TestCase):
+    def test_1_sample(self):
+        A=[0 for _ in range(12)]
+        A[0] = 1
+        A[1] = 5
+        A[2] = 3
+        A[3] = 4
+        A[4] = 3
+        A[5] = 4
+        A[6] = 1
+        A[7] = 2
+        A[8] = 3
+        A[9] = 4
+        A[10] = 6
+        A[11] = 2
+        self.assertEqual(solution(A), 3)
+    
+    # def test_2_sample(self):
+    #     self.assertEqual(solution([9, 3, 9, 3, 9, 9])  , 6)
+    
+    # def test_3_sample(self):
+    #     self.assertEqual(solution([1, 3, 1])  , 2)
+
+    # def test_4_sample(self):
+    #     self.assertEqual(solution([1, 1])  , 0)
+
+    # def test_3_sample(self):
+    #     self.assertEqual(solution([9, 8, 7, 5])  , 2)
+
+unittest.main(argv=['first-arg-is-ignored'], exit=False)
